82fea582a4ed6a7bd48830d53db3c9aa5d6e9550
galsp <gs3260474@gmail.com>
2023-10-04 14:06:27 +0700

    masih tahap beta

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-let a = prompt("masukan nilai")
-let b = prompt("pembagi") 
-let n = 1
-while(n < a){
-  console.log(n)
-  n = n * b
+function run(operator, bilangan1, bilangan2){
+  let hasil=0
+  if(operator == "+"){
+    hasil = bilangan1 + bilangan2
+  }
+  else if (operator == "-"){
+    hasil = bilangan1 - bilangan2
+  }
+  else if (operator == "*"){
+    hasil = bilangan1 * bilangan2
+  }
+  else if (operator == "/"){
+    if (bilangan2 !== 0 && bilangan1!==0){
+    hasil = bilangan1 / bilangan2
+    }
+    else{
+      hasil = "Eror"
+      
+    }
+  }
+  return(hasil);
+  
 }
+
+let akhir = run("/", 5, 1)
+console.log(akhir)
